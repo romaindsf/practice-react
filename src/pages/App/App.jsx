c99b5250f2f42d56a62d11840d62bc3a8d6ecfaa
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-03-16 17:46:05 +0100

    intiate react app

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <h1>Manipulating an array of object</h1>
+  )
+}
---
+++ src/pages/Champions_page/ChampionPage.jsx
@@ -0,0 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+export default function ChampionPage() {
+    const { championId } = useParams();
+
+
+    return (
+      <h1>{championId}</h1>
+    )
+  }
